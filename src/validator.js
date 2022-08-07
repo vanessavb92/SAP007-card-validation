fba0a26811d5393f61f04408eea7e621ebae720c
vanessa <vanessacamahuali28@gmail.com>
2022-08-07 20:33:42 -0300

    corrigindo erros de validação

--- src/validator.js
+++ src/validator.js
@@ -4,30 +4,25 @@ const validator = {
     maskify
 };
 
-// Número do cartão de máscara.
 function maskify(creditCardNumber) {
-    //Por meio da expressão regular após o quarto dígito eu começo a mascarar com # (Deixando os quatro últimos descobertos)
     let maskifyString = creditCardNumber.replace(/.(?=.{4})/g, "#");
-    return maskifyString //retorna a substituição dos dígitos por #
+    return maskifyString 
 }
 
-// Validar tarjeta.
+
 function isValid(creditCardNumber) {
-    // Eu converto a string em uma matriz, depois a inverto e mapeio convertendo a string em um número.
+// console.log(creditCardNumber.split("").reverse().map(Number));
+
     let arrayValidation = creditCardNumber.split('').reverse().map(Number);
-    //console.log(arrayValidation)
 
     const arrayNotmultiplied = arrayValidation.filter(function (_, index) {
         return index % 2 == 0;
     });
-    //console.log(`Retorna os números que não são multiplicados${arrayNotmultiplied}`)
 
     const arrayIfMultiplied = arrayValidation.filter(function (_, index) {
         return index % 2 !== 0;
     });
-    //console.log(`Retorna os números que são multiplicados ${arrayIfMultiplied}`)
 
-    // Multiplica os números x 2 e o resultado si resulta em dois digitos que somam entre si.
     const arrayMultiplicate = arrayIfMultiplied.map(function (element) {
         let num = element;
         num = num * 2;
@@ -36,18 +31,14 @@ function isValid(creditCardNumber) {
         }
         return num;
     });
-    //console.log(`Números multiplicados ${arrayMultiplicate}`)
 
     const arrayCombine = arrayNotmultiplied.concat(arrayMultiplicate);
 
-    //console.log(`Mostra os dois array juntos ${arrayCombine}`);
 
-    // Somar todos os números do array
     let sum = 0;
     for (let i = 0; i < arrayCombine.length; i++) {
         sum = sum + arrayCombine[i];
     }
-    // //console.log(som)
 
     if (sum % 10 == 0) {
         return true;
